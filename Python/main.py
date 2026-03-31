f09036e01a21f7155c7b83574d432ce0527ae0de
Sofiane <sofianefugon@icloud.com>
2026-03-31 17:52:46 +0200

    algo genetique

--- Python/main.py
+++ Python/main.py
@@ -1,13 +1,29 @@
-import PAESGISFAGRT  # 👈 important : R majuscule
+import PAESGISFAGRT
 
+print(PAESGISFAGRT.__file__)
+print(dir(PAESGISFAGRT))
 def oracle(x):
-    return 3*x**4 + 48*x**3 - 864*x**2
+    return (x + 8)**2 * (x + 1)**2 * (x - 6)**2 + 5*x
 
-res = PAESGISFAGRT.recuitsimule(
+
+res_gen = PAESGISFAGRT.genetique(
+    oracle,
+    100,   # taille_population
+    20,    # nb_elites
+    500,   # nb_generation
+    -100,  # domaine_min
+    100    # domaine_max
+)
+
+print("Résultat génétique :", res_gen)
+
+
+
+res_recuit = PAESGISFAGRT.recuitsimule(
     oracle,
     0.0001,
     1.0,
     0.99
 )
 
-print(res)
+print("Résultat recuit :", res_recuit)
--- Rust/src/lib.rs
+++ Rust/src/lib.rs
@@ -2,6 +2,82 @@ use pyo3::prelude::*;
 use rand::Rng;
 
 #[pyfunction]
+pub fn genetique(
+    py: Python,
+    oracle: PyObject,
+    taille_population: usize,
+    nb_elites: usize,
+    nb_generation: i64,
+    domaine_min: i64,
+    domaine_max: i64,
+) -> PyResult<i64> {
+
+    #[derive(Clone)]
+    struct Individu {
+        valeur: i64,
+        score: i64,
+    }
+
+    let mut population: Vec<Individu> = Vec::new();
+    let mut rng = rand::thread_rng();
+
+
+    for _ in 0..taille_population {
+        let tmp: i64 = rng.gen_range(domaine_min..domaine_max);
+
+        let score: i64 = oracle.call1(py, (tmp,))?.extract(py)?;
+
+        population.push(Individu {
+            valeur: tmp,
+            score,
+        });
+    }
+
+
+    for generation in 0..nb_generation {
+        population.sort_by_key(|individu| individu.score);
+
+        println!("Gen {} → best = {}", generation, population[0].valeur);
+
+        let meilleurs = &population[..nb_elites];
+        let mut newpopulation: Vec<Individu> = Vec::new();
+
+        // élites
+        for i in 0..nb_elites {
+            newpopulation.push(meilleurs[i].clone());
+        }
+
+
+        for _ in 0..(taille_population - nb_elites) {
+            let papa = &meilleurs[rng.gen_range(0..nb_elites)];
+            let maman = &meilleurs[rng.gen_range(0..nb_elites)];
+
+            let alpha: f64 = rng.r#gen();
+            let mut fiston = (alpha * papa.valeur as f64
+                + (1.0 - alpha) * maman.valeur as f64) as i64;
+
+            let facteur = 1.0 - (generation as f64 / nb_generation as f64);
+            let amplitude = (10.0 * facteur) as i64;
+
+            let mutation = rng.gen_range(-amplitude..=amplitude);
+            fiston += mutation;
+
+            let score: i64 = oracle.call1(py, (fiston,))?.extract(py)?;
+
+            newpopulation.push(Individu {
+                valeur: fiston,
+                score,
+            });
+        }
+
+        population = newpopulation;
+    }
+
+    population.sort_by_key(|individu| individu.score);
+
+    Ok(population[0].valeur)
+}
+#[pyfunction]
 pub fn recuitsimule(py: Python, oracle: PyObject, tmin: f64, t_init: f64,
     alpha: f64,
 ) -> PyResult<i64> {
@@ -59,5 +135,6 @@ pub fn recuitsimule(py: Python, oracle: PyObject, tmin: f64, t_init: f64,
 #[pymodule]
 fn PAESGISFAGRT(py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(recuitsimule, m)?)?;
+    m.add_function(wrap_pyfunction!(genetique, m)?)?;
     Ok(())
 }
